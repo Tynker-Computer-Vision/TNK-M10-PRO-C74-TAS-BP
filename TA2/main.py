af14bc7389c534e9c1d9ccd21a50b6a3fb3c8d5b
procodingclass <88050205+procodingclass@users.noreply.github.com>
2023-06-28 13:42:19 +0530

    Update main.py

--- TA2/main.py
+++ TA2/main.py
@@ -14,6 +14,8 @@ for img in os.listdir(path):
         # Check if img is not equal to .git i.e a wrong format image 
         
             # Get the age from the image path i.e img
+
+            #Append age to ages array
             
             # Read the image
             
